a742191580e688524c5fa74158ac639a106a8e8c
dj-idfx <35632851+dj-idfx@users.noreply.github.com>
2025-03-08 16:11:57 +0000

    Fix styling

--- src/ExampleServiceProvider.php
+++ src/ExampleServiceProvider.php
@@ -2,27 +2,26 @@
 
 namespace Idfx\Example;
 
-
+use Idfx\Example\Commands\ExampleCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Idfx\Example\Commands\ExampleCommand;
 
-class ExampleServiceProvider extends PackageServiceProvider{
-    public function configurePackage(Package $package): void{
-            /*
-             * This class is a Package Service Provider
-             *
-             * More info: https://github.com/spatie/laravel-package-tools
-             */
+class ExampleServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
 
+        $package
+            ->name('laravel-example-package')
+            ->hasConfigFile()
+            ->hasViews()
 
-            $package
-                ->name('laravel-example-package')
-                ->hasConfigFile()
-                ->hasViews()
-
-
-                ->hasMigration('create_laravel_example_package_table')
-                ->hasCommand(ExampleCommand::class);
+            ->hasMigration('create_laravel_example_package_table')
+            ->hasCommand(ExampleCommand::class);
     }
 }
