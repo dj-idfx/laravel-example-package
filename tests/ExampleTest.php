58085c51df74755983653b62aa7f2fe2a9e958cf
dj-idfx <35632851+dj-idfx@users.noreply.github.com>
2025-03-08 17:35:07 +0100

    Added test for ExampleCommand

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,13 @@
 <?php
 
+use Idfx\Example\Commands\ExampleCommand;
+use Illuminate\Console\Command;
+use function Pest\Laravel\artisan;
+
 it('can test', function () {
     expect(true)->toBeTrue();
+});
+
+it('can test ExampleCommand', function () {
+    artisan(ExampleCommand::class)->assertExitCode(Command::SUCCESS);
 });
