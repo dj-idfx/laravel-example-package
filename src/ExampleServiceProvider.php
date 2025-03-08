866a2e0def8b2e1cc3c64e7264a35e6b5ffaaae3
dj-idfx <35632851+dj-idfx@users.noreply.github.com>
2025-03-08 17:11:33 +0100

    Fix PHP code style issues - testrun

--- src/ExampleServiceProvider.php
+++ src/ExampleServiceProvider.php
@@ -2,24 +2,27 @@
 
 namespace Idfx\Example;
 
-use Idfx\Example\Commands\ExampleCommand;
+
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Idfx\Example\Commands\ExampleCommand;
 
-class ExampleServiceProvider extends PackageServiceProvider
-{
-    public function configurePackage(Package $package): void
-    {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-example-package')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_example_package_table')
-            ->hasCommand(ExampleCommand::class);
+class ExampleServiceProvider extends PackageServiceProvider{
+    public function configurePackage(Package $package): void{
+            /*
+             * This class is a Package Service Provider
+             *
+             * More info: https://github.com/spatie/laravel-package-tools
+             */
+
+
+            $package
+                ->name('laravel-example-package')
+                ->hasConfigFile()
+                ->hasViews()
+
+
+                ->hasMigration('create_laravel_example_package_table')
+                ->hasCommand(ExampleCommand::class);
     }
 }
