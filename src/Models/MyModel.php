2f1665b14293058494480ffe9ca9ba1f0e81139a
dj-idfx <35632851+dj-idfx@users.noreply.github.com>
2025-03-09 10:17:50 +0100

    Added $name property to MyModel

--- src/Models/MyModel.php
+++ src/Models/MyModel.php
@@ -5,6 +5,9 @@ namespace Idfx\Example\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $name
+ */
 class MyModel extends Model
 {
     use HasFactory;
